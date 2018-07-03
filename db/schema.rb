d8cf0f4cda84cdc2d3a6b8bbd0e850ac61744752
Donald Nguyen <dolnguyen@ucdavis.edu>
2018-07-02 17:34:45 -0700

    adds userId to retrieve exercises

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_03_001831) do
+ActiveRecord::Schema.define(version: 2018_07_03_003329) do
 
   create_table "exercises", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_07_03_001831) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "dayofweek"
+    t.integer "userId"
   end
 
 end
