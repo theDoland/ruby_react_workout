3ab572ef64e5ed74033cf2eeef77b8215baa1277
Donald Nguyen <dolnguyen@ucdavis.edu>
2018-08-04 21:37:53 -0700

    removed register service worker

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/Home';
-import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
